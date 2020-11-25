5184211a113ed6f37589f957a20ec6406fb5bb2c
Jack Weinbender <jack.weinbender@gmail.com>
2020-11-24 22:25:39 -0600

    Cleanup

--- init.sh
+++
@@ -1,6 +0,0 @@
-docker-compose build --no-cache && \
-docker-compose run web rails new . \
-    --force \
-    --database=postgresql && \
-docker-compose run web bundle install && \
-sudo chown -R $USER:$USER .
